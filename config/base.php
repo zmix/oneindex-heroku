476a832e2d340bb0ea1b0e200169ba7d46d5301d
unknown <pengfkun@gmail.com>
2019-08-21 02:47:21 +0800

    Initial commit

--- config/base.php
+++ config/base.php
@@ -1,19 +1,19 @@
 <?php return array (
   'site_name' => 'OneIndex',
-  'password' => 'PFKccmc1234sb',
-  'style' => 'nexmoe',
-  'onedrive_root' => '/',
+  'password' => 'oneindex',
+  'style' => 'material',
+  'onedrive_root' => '',
   'cache_type' => 'secache',
   'cache_expire_time' => 3600,
   'cache_refresh_time' => 600,
   'root_path' => '?',
-  'show' =>
+  'show' => 
   array (
-    'stream' =>
+    'stream' => 
     array (
       0 => 'txt',
     ),
-    'image' =>
+    'image' => 
     array (
       0 => 'bmp',
       1 => 'jpg',
@@ -21,16 +21,16 @@
       3 => 'png',
       4 => 'gif',
     ),
-    'video5' =>
+    'video5' => 
     array (
       0 => 'mp4',
       1 => 'webm',
       2 => 'mkv',
     ),
-    'video' =>
+    'video' => 
     array (
     ),
-    'video2' =>
+    'video2' => 
     array (
       0 => 'avi',
       1 => 'mpg',
@@ -43,13 +43,13 @@
       8 => 'ts',
       9 => 'flv',
     ),
-    'audio' =>
+    'audio' => 
     array (
       0 => 'ogg',
       1 => 'mp3',
       2 => 'wav',
     ),
-    'code' =>
+    'code' => 
     array (
       0 => 'html',
       1 => 'htm',
@@ -63,7 +63,7 @@
       9 => 'sh',
       10 => 'md',
     ),
-    'doc' =>
+    'doc' => 
     array (
       0 => 'csv',
       1 => 'doc',
@@ -85,11 +85,11 @@
       17 => 'xlsx',
     ),
   ),
-  'images' =>
+  'images' => 
   array (
     'home' => false,
     'public' => true,
-    'exts' =>
+    'exts' => 
     array (
       0 => 'jpg',
       1 => 'png',
@@ -97,10 +97,8 @@
       3 => 'bmp',
     ),
   ),
-  'client_secret' => 'pzzCUD0455?;cigeZLUX7[=',
-  'client_id' => '327ef1a0-ee66-43f9-bc52-9a3d1cd6aa58',
-  'redirect_uri' => 'https://fookdies303.herokuapp.com/',
-  'refresh_token' => 'OAQABAAAAAADCoMpjJXrxTq9VG9te-7FXQz2SlGAPI5qj8xS0T_UUuJ0gh13koZr4PEX425NDQ7vmO0m46pJLecT_WRBBSKKlOhIy4Tnk76rqJSh-aOjCjBKbT56txvADC0c6ySh3dbTS6ep1W7Tn3qKGa3nfA102RuDF6FB8WPmuHQj8_-JHf5lawbjAHvvSkJiLZ7MgZXtAyuKaes6gasEisGf1oQNnQ7tcLyHhmE2l1gFS7c_ke2ppLJBvvwCFv67tmBIUlYccRNESIhEbxtyCHaSo6M-OWoLRMA3nHC0W4enIUrwLj0OrZILRquQCKSFoTuOC8sqLPV5PX-IuqZJjpw1ngiRvkso8YzIv1fMlL2XC01MeeJ3XQ5TwfRZp-mP4RS4_uLF9b1ZveOmUyXnTf9oCE8-I2stxgPoRYPI_100VGyR52ZsQDTIr1Iclv95Bfo4fWL1Nr2TSyptHNfQiefs_d1kDB8Eggb54rqJlxD5s1x9GStJgdOHreY0VnXZfyoIpQSoplsNG8GYvXrJ9ZPiVhixO6xBmbsSpUGMaZtKieSGZOxe1ggo3daoRn5M0fR9fgX6IXZmvt-xMmwLGVl41dPdvGGJa56-d4IPBje0fqhK0FAhQk4I1LHlWrYCXKewIVYJGQcrtZ6-z8Z-0luJppPstgsXk1OdySQfMMFCCM7HsHQrH9zpSAffZu1pN7931my1eL5EP2CQelIJ0G2E_X2LxKb6tujWUjOOA-BCh0xLsQdZz1Iza-UjXVm4YJkJ3XKshXmmTK_16-eyRJwvbhgafkZXtwvimJ70BaF3VNDg09gGRoSlcHCFJUaocXzZx5hTbrfSofq9alD36XLngygTioaB6pfYmn8zH3O3RHhD5iiugQCAWqjkazpD4Ma5bGjcUYCpr8uYcY4d5B5Zg_BYqAn9hH66X47Xr0S5P1or2HcdWSjxtyzmGrV9QPs4M_TIgAA',
-  'onedrive_hide' => '',
-  'onedrive_hotlink' => '',
+  'client_secret' => 'eekePOWIM13487[hzzGC[}$',
+  'client_id' => 'ad934aa1-3198-4407-98ea-b7dbc4cb0186',
+  'redirect_uri' => 'https://oneindex.github.io/',
+  'refresh_token' => 'OAQABAAAAAAAP0wLlqdLVToOpA4kwzSnxi4O7a5_T1CfeCJnOpZKdTxLR8ojZ8dsRaUbsAMTG_0XVZdheqdSe-ihfb_CbPWu2QoeWUMvXHLuN8gl3fBcZxaVuGCKgJo3rD7Po2IuzUo8PcKR332LuXTIM0ao61ExpFXVw135zo483HEWMYMVEU0gA51gOLVzk5LROIm0ky-ru7oH83gFpz4Z-xH4pNUdemhByFGR2g67-IHqb4RTbPVPgh0nX-pPWaZu2gThYtmTc1Tj0ukNMhts_IRUfSS1TI9uTxqUh8DFT0WBayqeyUulr3s31nCMU0YziAQNUvQBU9lzelXSypyj_EK8hb9M9tWkvSoCtfziqsltch1GkrEGIGrTl3eaFOpUMku7grH5c7bx2tZGJw4kNbGbWQILJWHPLKY4SnPQiOgiORQq_Y0msMKbzkOb_FJeW2cj1B54-p7RVMxFU_yewNN8ks4o-ZBl9XoftqGOy_RUxaQUnA2T62n5wu5EzMOBaOJvkSFbDnespi4fDGEYrMziEfjC05L8pcldn2LjkkZ0VSLKtmtutDCm3CxX3h1JI8xkHSe3E4L0fR1f62IBKHC-GgAjT_8SIynHT21mEyb-EgqsdpA-SZd3kJYtEwhKnojToP0SaEdUufO5jmLFMJT7irul3lnlkBtKDMrh9ZdU8lDTn_tUcUnxBNFMdJtDAP-wnnxmCLjO-kA_Kf2737veQRK1HWm2L21pmkaRf6fVtBoHnA047umy26ytO2fyQ1lAfVUjTakYzum7710mVBDZYFQ6i0Sa0yaM7Zsu49BYTw_AW1TQfyNXIV9wsSUtf0mkmBk8kq42ZGERCs0Vejo5F8iMHR7EfI8kUod2Qj1xFrKAdT3kb76MYRADS45SO9Lk2YRlrhKW24_aa6gmw4jeqIbspG1gi_A0c5qJcpKxU_OxFI646yB3qT5hFzAMYYtz3TLJco0-g0RaX4agMP93L8hUwIAA',
 );
