02c35d7035f7a3af04e2a9977bc665bf9dc18f20
unknown <pengfkun@gmail.com>
2019-08-21 01:11:17 +0800

    first commit

---
+++ config/base.php
@@ -0,0 +1,106 @@
+<?php return array (
+  'site_name' => 'OneIndex',
+  'password' => 'PFKccmc1234sb',
+  'style' => 'nexmoe',
+  'onedrive_root' => '/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' =>
+  array (
+    'stream' =>
+    array (
+      0 => 'txt',
+    ),
+    'image' =>
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' =>
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' =>
+    array (
+    ),
+    'video2' =>
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' =>
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' =>
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' =>
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' =>
+  array (
+    'home' => false,
+    'public' => true,
+    'exts' =>
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'pzzCUD0455?;cigeZLUX7[=',
+  'client_id' => '327ef1a0-ee66-43f9-bc52-9a3d1cd6aa58',
+  'redirect_uri' => 'https://fookdies303.herokuapp.com/',
+  'refresh_token' => 'OAQABAAAAAADCoMpjJXrxTq9VG9te-7FXQz2SlGAPI5qj8xS0T_UUuJ0gh13koZr4PEX425NDQ7vmO0m46pJLecT_WRBBSKKlOhIy4Tnk76rqJSh-aOjCjBKbT56txvADC0c6ySh3dbTS6ep1W7Tn3qKGa3nfA102RuDF6FB8WPmuHQj8_-JHf5lawbjAHvvSkJiLZ7MgZXtAyuKaes6gasEisGf1oQNnQ7tcLyHhmE2l1gFS7c_ke2ppLJBvvwCFv67tmBIUlYccRNESIhEbxtyCHaSo6M-OWoLRMA3nHC0W4enIUrwLj0OrZILRquQCKSFoTuOC8sqLPV5PX-IuqZJjpw1ngiRvkso8YzIv1fMlL2XC01MeeJ3XQ5TwfRZp-mP4RS4_uLF9b1ZveOmUyXnTf9oCE8-I2stxgPoRYPI_100VGyR52ZsQDTIr1Iclv95Bfo4fWL1Nr2TSyptHNfQiefs_d1kDB8Eggb54rqJlxD5s1x9GStJgdOHreY0VnXZfyoIpQSoplsNG8GYvXrJ9ZPiVhixO6xBmbsSpUGMaZtKieSGZOxe1ggo3daoRn5M0fR9fgX6IXZmvt-xMmwLGVl41dPdvGGJa56-d4IPBje0fqhK0FAhQk4I1LHlWrYCXKewIVYJGQcrtZ6-z8Z-0luJppPstgsXk1OdySQfMMFCCM7HsHQrH9zpSAffZu1pN7931my1eL5EP2CQelIJ0G2E_X2LxKb6tujWUjOOA-BCh0xLsQdZz1Iza-UjXVm4YJkJ3XKshXmmTK_16-eyRJwvbhgafkZXtwvimJ70BaF3VNDg09gGRoSlcHCFJUaocXzZx5hTbrfSofq9alD36XLngygTioaB6pfYmn8zH3O3RHhD5iiugQCAWqjkazpD4Ma5bGjcUYCpr8uYcY4d5B5Zg_BYqAn9hH66X47Xr0S5P1or2HcdWSjxtyzmGrV9QPs4M_TIgAA',
+  'onedrive_hide' => '',
+  'onedrive_hotlink' => '',
+);
